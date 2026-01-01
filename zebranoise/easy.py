2d4560141b42f65e3f7f9c8717d145f8a4aefdd0
Max Shinn <max@maxshinnpotential.com>
2026-01-01 16:31:50 -0600

    Non-integer famerates, fixes #1

--- zebranoise/easy.py
+++ zebranoise/easy.py
@@ -2,6 +2,7 @@ import imageio
 from tqdm import tqdm
 import warnings
 from .util import generate_frames, filter_frames_index_function, apply_filters, discretize
+from math import ceil
 
 def zebra_noise(output_file, xsize, ysize, tdur, levels=10, xyscale=.2, tscale=50, fps=30, xscale=1.0, yscale=1.0, seed=0, filters=[("comb", 0.08)]):
     """Generate a .mp4 of zebra noise.
@@ -39,7 +40,7 @@ def zebra_noise(output_file, xsize, ysize, tdur, levels=10, xyscale=.2, tscale=5
     textra = (tscale - (tsize % tscale)) % tscale
     if textra > 0:
         warnings.warn(f"Adding {textra} extra timepoints to make tscale a multiple of tdur")
-    tsize += round(textra)
+    tsize += round(textra) if (textra % 1 < 1e-5) else ceil(textra)
     get_index = filter_frames_index_function(filters, tsize)
     writer = imageio.get_writer(output_file, fps=fps)
     for _i in tqdm(range(0, tsize)):
